161432e45ca601e46040d4fc01d705e57d7b6410
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@d537cd30-a800-f30c-cc8c-eaa9e1c31ce9>
2013-05-16 19:38:46 +0000

    Add bucket name validation function
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6431

--- trunk/python/src/cloudstorage/__init__.py
+++ trunk/python/src/cloudstorage/__init__.py
@@ -9,8 +9,14 @@ from storage_api import *
 
 
 try:
+  from .common import CSFileStat
+  from .common import GCSFileStat
+  from .common import validate_bucket_name
+  from .common import validate_bucket_path
+  from .common import validate_file_path
+except ImportError:
   from google.appengine.ext.cloudstorage.common import CSFileStat
   from google.appengine.ext.cloudstorage.common import GCSFileStat
-except ImportError:
-  from ..common import CSFileStat
-  from ..common import GCSFileStat
+  from google.appengine.ext.cloudstorage.common import validate_bucket_name
+  from google.appengine.ext.cloudstorage.common import validate_bucket_path
+  from google.appengine.ext.cloudstorage.common import validate_file_path
