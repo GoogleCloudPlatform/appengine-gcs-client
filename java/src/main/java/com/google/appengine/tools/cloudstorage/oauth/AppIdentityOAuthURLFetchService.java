ad49475193aec9aa298e88acdf3c05eb20bee1d5
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@d537cd30-a800-f30c-cc8c-eaa9e1c31ce9>
2014-01-22 19:52:06 +0000

    Java: Have the AppIdentityOAuthUrlFetchService be shared so that we have better caching and stagger expiration to reduce load on the appIdentityService.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6827

--- java/src/main/java/com/google/appengine/tools/cloudstorage/oauth/AppIdentityOAuthURLFetchService.java
+++ java/src/main/java/com/google/appengine/tools/cloudstorage/oauth/AppIdentityOAuthURLFetchService.java
@@ -22,6 +22,10 @@ import com.google.appengine.api.utils.SystemProperty;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * {@link OAuthURLFetchService} based on {@link AppIdentityService}. This will work in production
@@ -31,32 +35,81 @@ import java.util.List;
  */
 final class AppIdentityOAuthURLFetchService extends AbstractOAuthURLFetchService {
 
-  private static final int CACHE_EXPIRATION_HEADROOM = 120000;
+  /**
+   * A range of time is provided for the refresh so that multiple instance don't all attempt to
+   * refresh at the same time.
+   */
+  private static final int MAX_CACHE_EXPIRATION_HEADROOM = 300000;
+  private static final int MIN_CACHE_EXPIRATION_HEADROOM = 60000;
+  private final Random rand = new Random();
+  private final Object lock = new Object();
 
   private final AppIdentityService appIdentityService =
       AppIdentityServiceFactory.getAppIdentityService();
 
   private final List<String> oauthScopes;
 
-  private GetAccessTokenResult accessToken;
+  private final AtomicReference<GetAccessTokenResult> accessToken =
+      new AtomicReference<GetAccessTokenResult>();
+
+  private final AtomicInteger cacheExpirationHeadroom =
+      new AtomicInteger(getNextCacheExpirationHeadroom());
+
+  /**
+   * Used to prevent multiple requests from being issued in parallel from the same instance.
+   */
+  private final AtomicBoolean refreshInProgress = new AtomicBoolean(false);
 
   AppIdentityOAuthURLFetchService(URLFetchService urlFetch, List<String> oauthScopes) {
     super(urlFetch);
     this.oauthScopes = ImmutableList.copyOf(oauthScopes);
   }
 
+  /**
+   * Attempts to return the token from cache. If this is not possible because it is expired or was
+   * never assigned, a new token is requested and parallel requests will block on retrieving a new
+   * token. As such no guarantee of maximum latency is provided.
+   *
+   * To avoid blocking the token is refreshed before it's expiration, while parallel requests
+   * continue to use the old token.
+   */
   @Override
   protected String getToken() {
-
     if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Development) {
       throw new IllegalStateException(
           "The access token from the development environment won't work in production");
     }
-    if (accessToken == null || accessToken.getExpirationTime().getTime()
-        < System.currentTimeMillis() + CACHE_EXPIRATION_HEADROOM) {
-      accessToken = appIdentityService.getAccessToken(oauthScopes);
+    GetAccessTokenResult token = accessToken.get();
+    if (token == null || isExpired(token)
+        || (isAboutToExpire(token) && refreshInProgress.compareAndSet(false, true))) {
+      synchronized (lock) {
+        try {
+          token = accessToken.get();
+          if (token == null || isAboutToExpire(token)) {
+            token = appIdentityService.getAccessToken(oauthScopes);
+            accessToken.set(token);
+            cacheExpirationHeadroom.set(getNextCacheExpirationHeadroom());
+          }
+        } finally {
+          refreshInProgress.set(false);
+        }
+      }
     }
-    return accessToken.getAccessToken();
+    return token.getAccessToken();
+  }
+
+  private boolean isExpired(GetAccessTokenResult token) {
+    return token.getExpirationTime().getTime() < System.currentTimeMillis();
+  }
+
+  private boolean isAboutToExpire(GetAccessTokenResult token) {
+    return token.getExpirationTime().getTime() - cacheExpirationHeadroom.get()
+        < System.currentTimeMillis();
+  }
+
+  private final int getNextCacheExpirationHeadroom() {
+    return rand.nextInt(MAX_CACHE_EXPIRATION_HEADROOM - MIN_CACHE_EXPIRATION_HEADROOM)
+        + MIN_CACHE_EXPIRATION_HEADROOM;
   }
 
 }
