31f5d33dbe78877d6e05a6cfb92083decaadabc0
aozarov <ozarov@google.com>
2016-01-08 17:17:59 -0800

    Use URLConnection instead of URLFetch on VM Engine.

--- java/src/main/java/com/google/appengine/tools/cloudstorage/oauth/OauthRawGcsServiceFactory.java
+++ java/src/main/java/com/google/appengine/tools/cloudstorage/oauth/OauthRawGcsServiceFactory.java
@@ -16,11 +16,31 @@
 
 package com.google.appengine.tools.cloudstorage.oauth;
 
-import static com.google.appengine.api.urlfetch.URLFetchServiceFactory.getURLFetchService;
-
+import com.google.appengine.api.ThreadManager;
+import com.google.appengine.api.urlfetch.FetchOptions;
 import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.appengine.tools.cloudstorage.RawGcsService;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.io.ByteStreams;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -29,7 +49,7 @@ import com.google.common.collect.ImmutableSet;
 public final class OauthRawGcsServiceFactory {
 
   private static final AppIdentityOAuthURLFetchService appIdFetchService =
-      new AppIdentityOAuthURLFetchService(getURLFetchService(), OauthRawGcsService.OAUTH_SCOPES);
+      new AppIdentityOAuthURLFetchService(getUrlFetchService(), OauthRawGcsService.OAUTH_SCOPES);
 
   private OauthRawGcsServiceFactory() {}
 
@@ -39,5 +59,88 @@ public final class OauthRawGcsServiceFactory {
    */
   public static RawGcsService createOauthRawGcsService(ImmutableSet<HTTPHeader> headers) {
     return new OauthRawGcsService(appIdFetchService, headers);
+  }
+
+  private static URLFetchService getUrlFetchService() {
+    if (Boolean.parseBoolean(System.getenv("GAE_VM"))) {
+      return new URLConnectionAdapter();
+    }
+    return URLFetchServiceFactory.getURLFetchService();
+  }
+
+  private static class URLConnectionAdapter implements URLFetchService {
+
+    private static final ExecutorService executor =
+        new ThreadPoolExecutor(1, 100,
+            0L, TimeUnit.MILLISECONDS,
+            new SynchronousQueue<Runnable>(),
+            ThreadManager.currentRequestThreadFactory(),
+            new ThreadPoolExecutor.CallerRunsPolicy());
+
+    @Override
+    public HTTPResponse fetch(URL url) throws IOException {
+      return createHttpResponse((HttpURLConnection) url.openConnection());
+    }
+
+    @Override
+    public HTTPResponse fetch(HTTPRequest req) throws IOException {
+      URL url = req.getURL();
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod(req.getMethod().name());
+      for (HTTPHeader header : req.getHeaders()) {
+        connection.setRequestProperty(header.getName(), header.getValue());
+      }
+      FetchOptions fetchOptions = req.getFetchOptions();
+      connection.setInstanceFollowRedirects(fetchOptions.getFollowRedirects());
+      if (fetchOptions.getDeadline() != null) {
+        int timeout = (int) (fetchOptions.getDeadline() * 1000);
+        connection.setConnectTimeout(timeout);
+        connection.setReadTimeout(timeout);
+      } else {
+        connection.setConnectTimeout(20_000);
+        connection.setReadTimeout(30_000);
+      }
+      byte[] payload = req.getPayload();
+      if (payload != null) {
+        connection.setDoOutput(true);
+        OutputStream wr = connection.getOutputStream();
+        wr.write(payload);
+        wr.flush();
+        wr.close();
+      }
+      return createHttpResponse(connection);
+    }
+
+    private HTTPResponse createHttpResponse(HttpURLConnection connection) throws IOException {
+      final int responseCode = connection.getResponseCode();
+      final byte[] content =
+          ByteStreams.toByteArray(new BufferedInputStream(connection.getInputStream()));
+      List<HTTPHeader> headers =
+          Lists.newArrayListWithCapacity(connection.getHeaderFields().size());
+      for (Map.Entry<String, List<String>> h : connection.getHeaderFields().entrySet()) {
+        for (String v : h.getValue()) {
+          headers.add(new HTTPHeader(h.getKey(), v));
+        }
+      }
+      return new HTTPResponse(responseCode, content, null, headers);
+    }
+
+    @Override
+    public Future<HTTPResponse> fetchAsync(final URL url) {
+      return executor.submit(new Callable<HTTPResponse>() {
+        @Override public HTTPResponse call() throws Exception {
+          return fetch(url);
+        }
+      });
+    }
+
+    @Override
+    public Future<HTTPResponse> fetchAsync(final HTTPRequest request) {
+      return executor.submit(new Callable<HTTPResponse>() {
+        @Override public HTTPResponse call() throws Exception {
+          return fetch(request);
+        }
+      });
+    }
   }
 }
